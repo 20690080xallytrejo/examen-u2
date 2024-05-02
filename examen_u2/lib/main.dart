df71789744b0d24823e10d2544e6ded671dc3c70
20690080xallytrejo <20690080@tecvalles.mx>
2024-05-02 16:14:16 -0600

    Funcionamiento

--- examen_u2/lib/main.dart
+++ examen_u2/lib/main.dart
@@ -7,17 +7,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ONO',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Mini ONO'),
+      home: const MyHomePage(title: 'ONO'),
     );
   }
 }
@@ -32,10 +30,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   
+  int total = 0; 
+
   int randomNumber = Random().nextInt(9) + 1;
   int randomNum1 = Random().nextInt(9) + 1;
   int randomNum2 = Random().nextInt(9) + 1;
   int randomNum3 = Random().nextInt(9) + 1;
+  int computadora = Random().nextInt(9) + 1;
   
   int boxNumber = 0;
 
@@ -45,13 +46,51 @@ class _MyHomePageState extends State<MyHomePage> {
       randomNum1 = Random().nextInt(9) + 1; // Actualiza el número de la caja
     });
   }
+  void addToTotal(int number) {
+    setState(() {
+      total += number;
+    });
+  }
+
+
+void addToTotalAndUpdateNumbers(int number) {
+  setState(() {
+    total += number;
+    randomNumber = Random().nextInt(9) + 1;
+    computadora = Random().nextInt(9) + 1;
+  });
+}
+void addToTotalAndUpdateNumbers2(int number) {
+  setState(() {
+    total += number;
+    randomNum1 = Random().nextInt(9) + 1;
+    computadora = Random().nextInt(9) + 1;
+  });
+}
+void addToTotalAndUpdateNumbers3(int number) {
+  setState(() {
+    total += number;
+    randomNum2 = Random().nextInt(9) + 1;
+    computadora = Random().nextInt(9) + 1;
+  });
+}
+void addToTotalAndUpdateNumbers4(int number) {
+  setState(() {
+    total += number;
+    randomNum3 = Random().nextInt(9) + 1;
+    computadora = Random().nextInt(9) + 1;
+  });
+}
+
+
   @override
   Widget build(BuildContext context) {
-    double size = 200;
+    double size = 450.0;
+
     BoxDecoration cajaRedonda(Color fondo) => BoxDecoration(
         borderRadius: BorderRadius.circular(7),
         color: fondo,
-        border: Border.all(color: Colors.black45, width: 4));
+        );
 
     return Scaffold(
       appBar: AppBar(
@@ -62,71 +101,129 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            /* top */
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  decoration: cajaRedonda(Colors.blue),
-                  height: size,
-                  width: size / 2,
-                  child: Center(
-                    child: Text(
-                      '$randomNumber',
-                      style: const TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
-                      ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Container (
+                          child: Text('Numero de carta:'+ computadora.toString()),
+                        )
+                      ],
                     ),
-                  ),
+                  ],
                 ),
-                Container(
-                  decoration: cajaRedonda(Colors.red),
-                  height: size,
-                  width: size / 2,
-                  child: Center(
-                    child: Text(
-                      '$randomNum1',
-                      style: const TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
+                /* lado de la computadora */
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row( // Inicio de Row
+                      children: [ // Inicio de children
+                        Container(
+                          decoration: cajaRedonda(Colors.red),
+                          height: size / 3,
+                          width: size / 3,
+                        ),
+                        Container(
+                          decoration: cajaRedonda(Colors.yellow),
+                          height: size / 3,
+                          width: size / 3,
+                        ),
+                        Container(
+                          decoration: cajaRedonda(Colors.blue),
+                          height: size / 3,
+                          width: size / 3,
+                        ),
+                        Container(
+                          decoration: cajaRedonda(Colors.green),
+                          height: size / 3,
+                          width: size / 3,
+                        ),
+                      ], // Fin de children
+                    ), // Fin de Row
+                  ],
                 ),
-                Container(
-                  decoration: cajaRedonda(Colors.green),
-                  height: size,
-                  width: size / 2,
-                  child: Center(
-                    child: Text(
-                      '$randomNum2',
-                      style: const TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
-                      ),
+              ],
+            ),
+            /* button */
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Container (
+                          child: Text('GANADOR: ' + (total + computadora).toString()),
+                        )
+                      ],
                     ),
-                  ),
+                  ],
                 ),
-                Container(
-                  decoration: cajaRedonda(Colors.yellow),
-                  height: size,
-                  width: size / 2,
-                  child: Center(
-                    child: Text(
-                      '$randomNum3',
-                      style: const TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
+                /* lado del usuario */
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row( // Inicio de Row
+                      children: [ // Inicio de children
+                        GestureDetector(
+                          onTap: () => addToTotalAndUpdateNumbers(randomNumber),
+                          child: Container(
+                            decoration: cajaRedonda(Colors.red),
+                            height: size / 3,
+                            width: size / 3,
+                            child: Center(
+                              child: Text(randomNumber.toString()),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => addToTotalAndUpdateNumbers2(randomNum1),
+                          child: Container(
+                            decoration: cajaRedonda(Colors.orange),
+                            height: size / 3,
+                            width: size / 3,
+                            child: Center(
+                              child: Text(randomNum1.toString()),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => addToTotalAndUpdateNumbers3(randomNum2),
+                          child: Container(
+                            decoration: cajaRedonda(Colors.pink),
+                            height: size / 3,
+                            width: size / 3,
+                            child: Center(
+                              child: Text(randomNum2.toString()),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => addToTotalAndUpdateNumbers4(randomNum3),
+                          child: Container(
+                            decoration: cajaRedonda(Colors.green),
+                            height: size / 3,
+                            width: size / 3,
+                            child: Center(
+                              child: Text(randomNum3.toString()),
+                            ),
+                          ),
+                        ),
+                      ], // Fin de children
+                    ), // Fin de Row
+                  ],
                 ),
               ],
             ),
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
