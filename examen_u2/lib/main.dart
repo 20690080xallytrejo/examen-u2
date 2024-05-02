29b3cad6815dee51025d006b63f2092836fe9ca7
20690080xallytrejo <20690080@tecvalles.mx>
2024-05-02 15:32:16 -0600

    Datos

--- examen_u2/lib/main.dart
+++ examen_u2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Mini ONO',
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      //   useMaterial3: true,
-      // ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Mini ONO'),
     );
   }
 }
@@ -30,24 +31,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
+  int randomNumber = Random().nextInt(9) + 1;
+  int randomNum1 = Random().nextInt(9) + 1;
+  int randomNum2 = Random().nextInt(9) + 1;
+  int randomNum3 = Random().nextInt(9) + 1;
+  
+  int boxNumber = 0;
 
+  void updateBoxNumber() {
+    setState(() {
+      boxNumber = randomNum1; // Guarda el número de la caja
+      randomNum1 = Random().nextInt(9) + 1; // Actualiza el número de la caja
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    double size = 200;
+    BoxDecoration cajaRedonda(Color fondo) => BoxDecoration(
+        borderRadius: BorderRadius.circular(7),
+        color: fondo,
+        border: Border.all(color: Colors.black45, width: 4));
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: cajaRedonda(Colors.blue),
+                  height: size,
+                  width: size / 2,
+                  child: Center(
+                    child: Text(
+                      '$randomNumber',
+                      style: const TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: cajaRedonda(Colors.red),
+                  height: size,
+                  width: size / 2,
+                  child: Center(
+                    child: Text(
+                      '$randomNum1',
+                      style: const TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: cajaRedonda(Colors.green),
+                  height: size,
+                  width: size / 2,
+                  child: Center(
+                    child: Text(
+                      '$randomNum2',
+                      style: const TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: cajaRedonda(Colors.yellow),
+                  height: size,
+                  width: size / 2,
+                  child: Center(
+                    child: Text(
+                      '$randomNum3',
+                      style: const TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
